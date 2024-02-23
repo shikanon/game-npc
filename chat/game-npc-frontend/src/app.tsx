edd51f2176c9ef8d7d19ba0f5d6d3ba38f92a89d
ClarkChu <cc070194@gmail.com>
2024-02-23 17:28:19 +0800

    fix：更新域名配置

--- chat/game-npc-frontend/src/app.tsx
+++ chat/game-npc-frontend/src/app.tsx
@@ -22,7 +22,7 @@ export const request: RequestConfig = {
     {
       localhost: '/dev',
       'game-npc.clarkchu.com':
-        'http://game-npc.clarkchu.com:8000', // 云开发
+        'http://game-npc.clarkchu.com/api', // 云开发
     }[window.location.hostname] || '/',
   timeout: 120000, // 超时设置
   withCredentials: true,
